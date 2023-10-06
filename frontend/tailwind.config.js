bb4a3a15306eff62f9470b24f39f9977099feeee
Milind Nair <milindnair261003@gmail.com>
2023-10-07 00:39:17 +0530

    feat:tailwindcss config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 const withMT = require("@material-tailwind/react/utils/withMT");
  
-module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
+module.exports = withMT(
+  {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          bgprimaryColor : '#FFFF',
+          bgsecondaryColor : '#F7F7F7',
+          btnColor: '#1450A3',
+          iconColor: "#329D90"
+        },
+  
+        boxShadow: {
+          panelShadow: "rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;",
+        },
+      },
+    },
+    plugins: [],
+}
+);
