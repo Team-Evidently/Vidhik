93d594b8e41ce20efb52bf189d5df51acc54d9ad
Milind Nair <milindnair261003@gmail.com>
2023-10-07 19:05:04 +0530

    feat:added provider profile

---
+++ frontend/src/pages/lsp/LSP_Profile/Sidecard.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Sidecard = ({lawyer}) => {
+    return (
+        <div className='shadow-panelShadow p-3 lg:p-5 rounded-md lg:h-[40vh]  '>
+            <div className="flex items-center justify-between">
+                <p className="text__para mt-0 font-semibold">Avg Hearing Price </p>
+                <span className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold'>
+                   {lawyer.avg_price_range_per_hearing.map((price, index) => (
+                       <span key={index}> ${index==0 ? (price + '-'):(price)}</span>
+                   ))}
+                </span>
+            
+            </div>
+            <span className="text-[12px] leading-5 text-gray-500">(Variable upon discussion)</span>
+            <div className='mt-[30px]'>
+                <p className="text__para mt-0 font-semibold text-headingColor">
+                    Available Time Slots:
+                </p>
+                <ul className="mt-3">
+                    <li className="flex items-center justify-between mb-2">
+                        <p className="text-[15px] leading-6 text-textColor font-semibold">Sunday</p>
+                        <p className="text-[15px] leading-6 text-textColor font-semibold">4:00 PM - 9:30PM</p>
+
+                    </li>
+                    <li className="flex items-center justify-between mb-2">
+                        <p className="text-[15px] leading-6 text-textColor font-semibold">Tuesday</p>
+                        <p className="text-[15px] leading-6 text-textColor font-semibold">4:00 PM - 9:30PM</p>
+
+                    </li>
+                    <li className="flex items-center justify-between mb-2">
+                        <p className="text-[15px] leading-6 text-textColor font-semibold">Wednesday</p>
+                        <p className="text-[15px] leading-6 text-textColor font-semibold">4:00 PM - 9:30PM</p>
+
+                    </li>
+                </ul>
+            </div>
+            <button className="btn px-2 w-full rounded-md">Request an Appointment</button>
+            <button className="btn px-2 w-full rounded-md mt-4">ek aur socho</button>
+        </div>
+    )
+}
+
+export default Sidecard
